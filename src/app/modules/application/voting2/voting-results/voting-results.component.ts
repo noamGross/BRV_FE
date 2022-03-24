14515fd45849490f4e337f52475b356a2262975b
noam.g <n.gross@tase.co.il>
2022-03-24 18:20:33 +0200

    add Voting result not operetional

---
+++ src/app/modules/application/voting2/voting-results/voting-results.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-voting-results',
+  templateUrl: './voting-results.component.html',
+  styleUrls: ['./voting-results.component.css']
+})
+export class VotingResultsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
